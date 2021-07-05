8341a40e5834d20302c8588898bd5f3e7c526adf
Legendarytruth <65697867+Legendarytruth@users.noreply.github.com>
2021-07-05 02:28:16 -0400

    Finished Assignment
    Fulfill all tests

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const exerciseSchema = new Schema({
   duration: { type: Number, required: true },
   user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   date: { type: String},
-
+  other_date: { type: Date, default: Date.now },
 },{ versionKey: false });
 
 let User = mongoose.model('User', userSchema);
@@ -61,15 +61,18 @@ async function createExercise(ids, desc, dur, dat){
       var now = new Date();
       var day = days[now.getDay()];
       var month = months[now.getMonth()];
-      var t = day +" "+ month+ " "+ now.getDate() + " " + now.getFullYear()
-      let e = new Exercise({username: test.username, description:desc, duration: dur, user_id: ids,date: t})
+      var date = parseInt(now.getDate()) < 10 ? "0"+now.getDate() : now.getDate();
+      //console.log(date)
+      var t = day +" "+ month+ " "+ date + " " + now.getFullYear()
+      let e = new Exercise({username: test.username, description:desc, duration: dur, user_id: ids,date: t, other_date: now})
       return e.save()
     }else{
       var now = new Date(dat)
       var day = days[now.getDay()];
       var month = months[now.getMonth()];
-      var t = day +" "+ month+ " "+ now.getDate() + " " + now.getFullYear()
-      let e = new Exercise({username: test.username, description:desc, duration: dur, user_id: ids,date: t})
+      var date = parseInt(now.getDate()) < 10 ? "0"+now.getDate() : now.getDate();
+      var t = day +" "+ month+ " "+ date + " " + now.getFullYear()
+      let e = new Exercise({username: test.username, description:desc, duration: dur, user_id: ids,date: t, other_date: now})
       return e.save()
 
     }
@@ -101,15 +104,50 @@ app.post("/api/users/:_id/exercises", async function(req, res){
   if(e == "error"){
     res.json({error: "Unknown userId"})
   }else{
-    var s = {username: e.username, description: e.description, duration: e.duration, _id: e.user_id, date: e.date}
+    var s = {"_id": req.params._id, "username": e.username, "date": e.date, "duration": e.duration , "description":e.description}
+    //console.log(s)
+    //{username: e.username, description: e.description, duration: e.duration, _id: e.user_id, date: e.date}
     //console.log(s)
     res.json(s)
   }
 })
 
-app.get('/api/users/:_id/logs', (req, res) => {
-  console.log(req.query.from)
-  console.log(req.query.to)
+app.get('/api/users/:_id/logs', (req, res) =>{
+  let filter_date = {};
+  console.log(req.query.to +" / " + req.query.from +" / " + req.query.limit + " / "+ req.params._id)
+  if(req.query.from){
+    filter_date["$gte"] = new Date(req.query.from);
+  }
+  if(req.query.to){
+    filter_date["$lte"] = new Date(req.query.to);
+  }
+  User.findById(req.params._id).then((u) =>{ 
+  let u_obj = u.toObject();
+  let exer = {};
+  if(!(Object.keys(filter_date).length === 0 && filter_date.constructor === Object)){
+    //console.log(filter_date)
+    exer = Exercise.find({
+    user_id: req.params._id, other_date: filter_date
+  });
+  }else{
+      exer = Exercise.find({
+    user_id: req.params._id,
+  });
+  }
+  //exer.exec((err, e) => console.log(e))
+  if(req.query.limit){
+    exer = exer.limit(parseInt(req.query.limit));
+  }
+  exer.exec((error, e) => {
+        if (error) {
+          res.json({ error: error });
+        } else {
+          u_obj.count = e.length;
+          u_obj.log = e;
+          //console.log(u_obj)
+          res.json(u_obj)
+}})    
+})
 });
 
 
